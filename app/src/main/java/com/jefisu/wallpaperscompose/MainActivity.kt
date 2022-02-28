57f07ba7178b325bc871fcda17a006d857b37554
Jeferson Coutinho <jefissuu@gmail.com>
2022-02-28 19:49:55 -0300

    Implemented splash screen

--- app/src/main/java/com/jefisu/wallpaperscompose/MainActivity.kt
+++ app/src/main/java/com/jefisu/wallpaperscompose/MainActivity.kt
@@ -3,20 +3,29 @@ package com.jefisu.wallpaperscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.rememberPagerState
+import com.jefisu.wallpaperscompose.presentation.viewmodel.SplashViewModel
 import com.jefisu.wallpaperscompose.util.Navigation
 
 @ExperimentalAnimationApi
 @ExperimentalPagerApi
 @ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().setKeepOnScreenCondition {
+            viewModel.isLoading.value
+        }
         setContent {
             val navController = rememberNavController()
             val pagerState = rememberPagerState()
